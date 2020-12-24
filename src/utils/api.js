06139ed4082d6bfe7a17d6cc68d68a8e9260ac41
Muhamad Istiady Kartadibrata <mkartadibrata@gmail.com>
2020-12-24 13:21:57 +0700

    [edit] change api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 const head = { "Content-Type": "application/json", Accept: "application/json" };
 
-const baseUrl = "http://localhost:8080/api";
+const baseUrl = "http://chulsan-api.herokuapp.com/api";
 
 export function headers() {
   return {
